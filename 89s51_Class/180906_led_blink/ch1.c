97ecebd038d67bcf9336627cd0e773f99e3de1bd
a34007024 <a34007024@gmail.com>
2018-09-06 11:42:47 +0800

    180906 led blink practice 2
    練習2

--- 89s51_Class/180906_led_blink/ch1.c
+++ 89s51_Class/180906_led_blink/ch1.c
@@ -3,17 +3,32 @@
 void delay(int);
 
 void main(){
-	char pat = 0x0f;
+	char pat = 0xff;
 	led = pat;
 	while(1){
-		delay(250);
-		led =~ led;
+		led = 0xff;
+		delay(500);
+		led = 0xe7;
+		delay(500);
+		led = 0xc3;
+		delay(500);
+		led = 0x81;
+		delay(500);
+		led = 0x00;
+		delay(500);
 		//=~代表把變數反相	
 	}
 }
-
+/* blink table (0 = bright 1 = dark)
+1111 1111 0xff
+1110 0111 0xe7
+1100 0011 0xc3
+1000 0001 0x81
+0000 0000 0x00
+*/
 void delay(int millisecond){
-	for(int i=0;i < millisecond;i++){
-		for(int j=0;j<120;j++)
+	int i,j;
+	for(i=0;i < millisecond;i++){
+		for(j=0;j<120;j++);
 	}
 }
