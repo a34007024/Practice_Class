7f01e1ece4cf64fde2aaabceb0013c984ab0fa0f
a34007024 <a34007024@gmail.com>
2018-09-06 14:40:49 +0800

    update ch2-1.c

--- 89s51_Class/180906_buzzer/ch2-1.c
+++ 89s51_Class/180906_buzzer/ch2-1.c
@@ -1,29 +1,56 @@
 #include <reg51.h>
 sbit buzzer=P3^7;
 sbit buzzer_sw=P0^0;
+sbit buzzer_sw2=P0^1;
 void beep(int);
-void delay500us(int);
+void beepSW2(int);
+void delay100us(int);
 
 void main(){
 	buzzer = 1;buzzer_sw=1;
 	//set buzzer and switch to OFF at startup
 	while(1){
-		if(!buzzer_sw) beep(1);
+		if(!buzzer_sw)beep(1);	// if(!buzzer_sw)  =  if(buzzer_sw==0)
+		if(!buzzer_sw2)beepSW2(1);
 	}
 }
 void beep(int beepcount){//one beep last for 0.2s
 	int i,j;
 	for(i=0;i<beepcount;i++){
 		for(j=0;j<100;j++){
-			buzzer = 0;delay500us(1);
-			buzzer = 1;delay500us(1);
+			buzzer = 0;delay100us(1);
+			buzzer = 1;delay100us(1);
 		}
-		delay500us(200); //100ms
+		delay100us(1000); //100ms
+	}  
+	for(i=0;i<beepcount;i++){
+		for(j=0;j<100;j++){
+			buzzer = 0;delay100us(2);
+			buzzer = 1;delay100us(2);
+		}
+		delay100us(1000); //100ms
 	}
 }
-void delay500us(int time){
+void beepSW2(int beepcount){//one beep last for 0.2s
+	int i,j;
+	for(i=0;i<beepcount;i++){
+		for(j=0;j<100;j++){
+			buzzer = 0;delay100us(3);
+			buzzer = 1;delay100us(3);
+		}
+		delay100us(1000); //100ms
+	}  
+	for(i=0;i<beepcount;i++){
+		for(j=0;j<100;j++){
+			buzzer = 0;delay100us(5);
+			buzzer = 1;delay100us(5);
+		}
+		delay100us(1000); //100ms
+	}
+}
+void delay100us(int time){
 	int i,j;
 	for(i=0;i<time;i++){
-		for(j=0;j<60;j++);
+		for(j=0;j<12;j++);
 	}
 }
