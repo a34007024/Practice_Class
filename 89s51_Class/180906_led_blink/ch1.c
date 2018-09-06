9c3e50416447a3bc1919e5788491548357d7f60f
a34007024 <a34007024@gmail.com>
2018-09-06 11:08:50 +0800

    gitignore update and classify some folder and files

---
+++ 89s51_Class/180906_led_blink/ch1.c
@@ -0,0 +1,18 @@
+#include <reg51.h>
+#define led P1
+void delay(int);
+
+void main(){
+	char pat = 0x0f;
+	led = pat;
+	while(1){
+		led =~ led;
+		//=~代表把變數反相	
+	}
+}
+
+void delay(int millisecond){
+	for(int i=0;i < millisecond;i++){
+		for(int j=0;j<120;j++)
+	}
+}
