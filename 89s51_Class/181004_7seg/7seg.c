600ca7098242a5515f755dd8a68d890cc434fa21
郵差 <a34007024@gmail.com>
2018-10-04 11:25:37 +0800

    7seg show 0~9 char array hexCode version

--- 89s51_Class/181004_7seg/7seg.c
+++ 89s51_Class/181004_7seg/7seg.c
@@ -1,6 +1,7 @@
 #include<reg51.h>
 #define seg7 P2
 sbit seg7dp = P2^7;
+char seg7hexCode[16] = {0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x98};
 /*
 接線說明:
 a -> P2.0 (P2^0)
@@ -8,32 +9,26 @@ b -> P2.1
 依序a,b,c,d,e,f,g......
 最後為dp -> P2.7
 */
-void segShow(int number,int dotEnable);
 void delay(int ms);
 void main(){
 	while(1){
-		segShow(0,1);delay(300);
-		segShow(1,1);delay(300);
-		segShow(2,1);delay(300);
-		segShow(3,1);delay(300);
-		segShow(4,1);delay(300);
-		segShow(5,1);delay(300);
-		segShow(6,1);delay(300);
-		segShow(7,1);delay(300);
-		segShow(8,1);delay(300);
-		segShow(9,1);delay(300);
-		segShow(-1,1);delay(1000);
-		segShow(0,0);delay(300);
-		segShow(1,0);delay(300);
-		segShow(2,0);delay(300);
-		segShow(3,0);delay(300);
-		segShow(4,0);delay(300);
-		segShow(5,0);delay(300);
-		segShow(6,0);delay(300);
-		segShow(7,0);delay(300);
-		segShow(8,0);delay(300);
-		segShow(9,0);delay(300);
-		segShow(-1,1);delay(1000);
+		int i = 0;
+		for(i = 0;i<10;i++){
+			seg7 = seg7hexCode[i];
+			delay(300);
+			seg7 = 0xff;//turn off 7seg
+			delay(300);
+		}
+		seg7 = 0xff;
+		delay(1000);
+		for(i = 9;i>=0;i--){
+			seg7 = seg7hexCode[i];
+			delay(300);
+			seg7 = 0xff;//turn off 7seg
+			delay(300);
+		}
+		seg7 = 0xff;
+		delay(1000);
 	}
 }
 
@@ -41,44 +36,4 @@ void delay(int ms){
 	int i,j;
 	for(i = 0;i < ms;i++)
 		for(j=0;j<120;j++);//do null
-}
-
-void segShow(int number,int dotEnable){
-	switch(number){
-		case 0:
-			seg7 = 0xc0;
-			break;
-		case 1:
-			seg7 = 0xf9;
-			break;
-		case 2:
-			seg7 = 0xa4;
-			break;
-		case 3:
-			seg7 = 0xb0;
-			break;
-		case 4:
-			seg7 = 0x99;
-			break;
-		case 5:
-			seg7 = 0x92;
-			break;
-		case 6:
-			seg7 = 0x82;
-			break;
-		case 7:
-			seg7 = 0xf8;
-			break;
-		case 8:
-			seg7 = 0x80;
-			break;
-		case 9:
-			seg7 = 0x98;
-			break;
-		default:
-			seg7 = 0xff;
-			break;
-	}
-	if(dotEnable == 0) seg7dp = 0;
-	else seg7dp = 1;
 }
