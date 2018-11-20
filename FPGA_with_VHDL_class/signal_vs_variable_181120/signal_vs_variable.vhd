7b5265d9c23e287a88dd8a2f5c8160c16b768750
郵差 <a34007024@gmail.com>
2018-11-20 14:45:43 +0800

    signal_vs_variable -- variable version

---
+++ FPGA_with_VHDL_class/signal_vs_variable_181120/signal_vs_variable.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+---------------------------------
+entity signal_vs_variable is 
+	port(	clk:in std_logic;
+			B:out integer range 0 to 15);
+end entity;
+--此文件用來練習、比較signal 和 variable的差異性
+architecture main of signal_vs_variable is
+--signal A:integer range 0 to 15 := 0;
+begin
+	process(clk)
+	variable A:integer range 0 to 15:=0;
+	begin
+		if rising_edge(clk) then
+			A := A + 1;
+			B <= A;
+			if A=5 then 
+				A := 0;
+			end if;
+		end if;
+	end process;
+
+end architecture;
