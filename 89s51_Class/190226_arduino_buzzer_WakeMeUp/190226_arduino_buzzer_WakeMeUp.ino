21696feda31fa4d645ced518df742b89c7a0d808
Luckypig <a34007024@gmail.com>
2019-02-26 22:37:04 +0800

    finish first version!
    this version is needed for human finger to control ~!!

--- 89s51_Class/190226_arduino_buzzer_WakeMeUp/190226_arduino_buzzer_WakeMeUp.ino
+++ 89s51_Class/190226_arduino_buzzer_WakeMeUp/190226_arduino_buzzer_WakeMeUp.ino
@@ -1,24 +1,30 @@
 #include "pitches.h"
 #include "music.h"
-
-const int tonePin=9;//蜂鳴器的pin
+//使用100歐姆以上電阻
+const int btnPin = 49;
+const int tonePin = 11; //蜂鳴器的pin
 int length;//這裡定義一個變數，後面用來表示共有多少個音符
 void setup()
 {
-  pinMode(tonePin,OUTPUT);//設置蜂鳴器的pin為輸出模式
-  length = sizeof(tune)/sizeof(tune[0]);
+  pinMode(tonePin, OUTPUT); //設置蜂鳴器的pin為輸出模式
+  pinMode(btnPin, INPUT);
+  length = sizeof(tune) / sizeof(tune[0]);
   //這裡用了一個sizeof函式，可以查出tone序列裡有多少個音符
+  pinMode(45,OUTPUT);//按鈕的VCC
+  digitalWrite(45,HIGH);
 }
-
+int noteCount = 0;
 void loop()
 {
-  for(int x=0;x<length;x++)//迴圈音符的次數
-  {
-    tone(tonePin,tune[x]);//此函數依次播放tune序列裡的陣列，即每個音符
-    delay(400);
-    //delay(400*duration[x]);//每個音符持續的時間，即節拍duration，
-    //400是調整音符的持續時間，數值越大，曲子速度越慢，越小曲子速度越快，自己掌握吧
-    noTone(tonePin);//停止當前音符，進入下一音符
+  if(noteCount >= length - 1)noteCount = 0;
+  if (digitalRead(btnPin) == 1) {
+    delay(3);//反彈跳
+    if (digitalRead(btnPin) == 1) {
+      while (digitalRead(btnPin) == 1) {
+        tone(tonePin, tune[noteCount]); //此函數依次播放tune序列裡的陣列，即每個音符
+      }
+      noTone(tonePin);//停止當前音符，進入下一音符
+      noteCount += 1;
+    }
   }
-  delay(5000);//等待5秒後，迴圈重新開始
 }
